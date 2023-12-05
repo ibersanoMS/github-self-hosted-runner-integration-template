a3c59630c9a8f054c289e2a2a12f24c50766dfdd
Isabelle Bersano <ibersano@microsoft.com>
2023-12-05 16:16:00 -0500

    fix: typos

--- src/infra/terraform/runner-infra/acr/main.tf
+++ src/infra/terraform/runner-infra/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "runnerContainerRegistry" {
-  name                     = "runnerContainerRegistry"
+  name                     = "runnercontainerregistry"
   resource_group_name      = var.resourceGroupName
   location                 = var.location
   sku                      = "Standard"
